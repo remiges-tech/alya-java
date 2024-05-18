1d9c48db85df3720eeb87e9ab2ce645a9f3b164b
meghajadhav <megha.jadhav@remiges.tech>
2024-05-18 16:17:03 +0530

    refs: #19049 : Multilingual Text Indexs

--- Alya_ValidationLib/projects/ngx-alyavalidation/src/lib/ngx-alyavalidation.module.ts
+++ Alya_ValidationLib/projects/ngx-alyavalidation/src/lib/ngx-alyavalidation.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { NgxAlyavalidationComponent } from './ngx-alyavalidation.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 
 
@@ -8,9 +11,22 @@ import { NgxAlyavalidationComponent } from './ngx-alyavalidation.component';
     NgxAlyavalidationComponent
   ],
   imports: [
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   exports: [
     NgxAlyavalidationComponent
   ]
 })
 export class NgxAlyavalidationModule { }
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+  
+}
