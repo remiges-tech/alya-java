60567ae1fdfbad772d9265f361a888574a64ffcd
prasadn <prasad.nikam@remiges.tech>
2024-05-31 17:54:30 +0530

    refs: #18627 - Slow Queries WSC test run implemented output DTO and initialization of slow query processor method

---
+++ demoalya/src/main/java/com/remiges/demoalya/component/SlowQueryProcessor.java
@@ -0,0 +1,15 @@
+package com.remiges.demoalya.component;
+
+import java.util.concurrent.Future;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.remiges.alya.jobs.BatchInitBlocks;
+
+public class SlowQueryProcessor {
+    public Future<BatchResult> processSlowQuery(BatchInitBlocks initBlock, JsonNode context, String input) {
+		return null;
+    	
+    }
+
+
+}
