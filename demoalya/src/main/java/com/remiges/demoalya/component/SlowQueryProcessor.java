d1b160dc964a00f39f23c408b5d2697032d40a64
prasadn <prasad.nikam@remiges.tech>
2024-06-02 19:06:10 +0530

    refs: #18627 - Slow Queries WSC test run implemented SlowQuery Proccessor doSlowQuery function

--- demoalya/src/main/java/com/remiges/demoalya/component/SlowQueryProcessor.java
+++ demoalya/src/main/java/com/remiges/demoalya/component/SlowQueryProcessor.java
@@ -6,18 +6,20 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.Future;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.remiges.alya.jobs.BatchInitBlocks;
 import com.remiges.alya.jobs.BatchOutput;
 import com.remiges.alya.jobs.BatchStatus;
 import com.remiges.alya.jobs.ErrorCodes;
+import com.remiges.alya.jobs.SQProcessor;
 
 import redis.clients.jedis.Jedis;
 
-public class SlowQueryProcessor {
-	public BatchOutput processSlowQuery(BatchInitBlocks initBlock, JsonNode context, String input) {
+public class SlowQueryProcessor implements SQProcessor {
+
+	@Override
+	public BatchOutput DoSlowQuery(BatchInitBlocks initBlock, JsonNode context, String input) {
 		Map<String, String> blobRows = new HashMap<>();
 		String messages = "";
 
@@ -46,6 +48,6 @@ public class SlowQueryProcessor {
 		// Now you have the query output stored in the blobRows map
 		// You can perform further processing or return it as needed
 
-        return new BatchOutput(BatchStatus.BatchSuccess, null, null, blobRows, ErrorCodes.NOERROR);
+		return new BatchOutput(BatchStatus.BatchSuccess, null, null, blobRows, ErrorCodes.NOERROR);
 	}
 }
